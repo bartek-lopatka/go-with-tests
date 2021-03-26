90915a94331b01ed478211f01d7e66e8dd5bac37
bartek.lopatka <bartek.lopatka@jamf.com>
2021-03-26 13:17:17 +0100

    Added real function code + benchmark

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,5 +1,11 @@
 package iteration
 
-func Repeat(a string) string {
-	return a
+const repeatCount = 4
+
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
 }
