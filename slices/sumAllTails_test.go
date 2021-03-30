61aaf1a0f3a93c28119f684241fe6b9ff71ea29c
bartek.lopatka <bartek.lopatka@jamf.com>
2021-03-30 10:46:33 +0200

    Moved sum checking into a helper function

--- slices/sumAllTails_test.go
+++ slices/sumAllTails_test.go
@@ -7,21 +7,25 @@ import (
 
 func TestSumAllTails(t *testing.T) {
 
-	t.Run("make the sums of some slices", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2}, []int{5, 7})
-		want := []int{2, 7}
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v, wanted %v", got, want)
 		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{5, 7})
+		want := []int{2, 7}
+
+		checkSums(t, got, want)
 	})
 
 	t.Run("safely sum with empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{5, 7, 8})
 		want := []int{0, 15}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v, wanted %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
