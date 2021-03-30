579f55cd91b28596703819ee4de5d49060d8ff3f
bartek.lopatka <bartek.lopatka@jamf.com>
2021-03-30 10:36:25 +0200

    Added sumAllTails and test for empty slice

---
+++ slices/sumAllTails.go
@@ -0,0 +1,15 @@
+package slices
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) >= 1 {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+	return sums
+}
