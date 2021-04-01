cf95c50a3b2595771e45ae51e7df309c2160fe57
bartek.lopatka <bartek.lopatka@jamf.com>
2021-04-01 10:27:25 +0200

    Added area and perimeter functions

---
+++ structs/geometry.go
@@ -0,0 +1,9 @@
+package structs
+
+func Perimeter(length float64, width float64) float64 {
+	return 2 * (length + width)
+}
+
+func Area(length float64, width float64) float64 {
+	return length * width
+}
