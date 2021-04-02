e308de33ec0860d882952510ab235e0887a95f34
bartek.lopatka <bartek.lopatka@jamf.com>
2021-04-02 10:29:15 +0200

    Changed to table driven tests

--- structs/geometry_test.go
+++ structs/geometry_test.go
@@ -13,22 +13,33 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-
-	checkArea := func(tb testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
-		}
-
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 4.0}, 40.0},
+		{Circle{10.0}, 314.1592653589793},
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{6.0, 4.0}
-		checkArea(t, rectangle, 24.0)
-	})
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g, want %g", got, tt.want)
+		}
+	}
+	//checkArea := func(tb testing.TB, shape Shape, want float64) {
+	//	t.Helper()
+	//	got := shape.Area()
+	//	if got != want {
+	//		t.Errorf("got %g, want %g", got, want)
+	//	}
+	//}
+	//t.Run("rectangles", func(t *testing.T) {
+	//	rectangle := Rectangle{6.0, 4.0}
+	//	checkArea(t, rectangle, 24.0)
+	//})
+	//t.Run("circles", func(t *testing.T) {
+	//	circle := Circle{10.0}
+	//	checkArea(t, circle, 314.1592653589793)
+	//})
 }
