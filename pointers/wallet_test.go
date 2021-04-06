750b09805bd73a93935ec21abf69535d5febfb7a
bartek.lopatka <bartek.lopatka@jamf.com>
2021-04-06 11:39:17 +0200

    Added testing for withdrawing only sufficient amount of money

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -4,24 +4,38 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
-	asssertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
 		t.Helper()
 		got := wallet.balance
 		if got != want {
 			t.Errorf("got %s, want %s", got, want)
 		}
-
+	}
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Errorf("Wanted an error, but didn't get one")
+		}
 	}
 
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-		asssertBalance(t, wallet, Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{20}
 		wallet.Withdraw(Bitcoin(10))
-		asssertBalance(t, wallet, Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err)
 	})
 }
