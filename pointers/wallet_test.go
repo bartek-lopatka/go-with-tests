4964fec0b924c48a3e60c100736a869c6b0032f9
bartek.lopatka <bartek.lopatka@jamf.com>
2021-04-06 10:52:42 +0200

    Refactored tests and got rid of repeated code

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,14 +3,25 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+	asssertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.balance
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %s, want %s", got, want)
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		asssertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{20}
+		wallet.Withdraw(Bitcoin(10))
+		asssertBalance(t, wallet, Bitcoin(10))
+	})
 }
