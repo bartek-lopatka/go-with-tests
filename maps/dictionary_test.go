16d5d66901da361379ebce3b5571377ff8f00ea1
bartek.lopatka <bartek.lopatka@jamf.com>
2021-04-07 11:40:53 +0200

    Added error handling for duplicate items in the dict

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -19,13 +19,28 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "mykey"
-	definition := "myvalue"
+	t.Run("new word", func(t *testing.T) {
 
-	dictionary.Add(word, definition)
+		dictionary := Dictionary{}
+		word := "mykey"
+		definition := "myvalue"
 
-	assertDefinition(t, dictionary, word, definition)
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "mykey"
+		definition := "myvalue"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "something new")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
