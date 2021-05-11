0d497285d4978f875626094ef7b447c7a107e384
bartek.lopatka <bartek.lopatka@jamf.com>
2021-05-11 16:52:07 +0200

    Print 3 2 1 Go! in separate lines, moves values to const

--- mocking/mock.go
+++ mocking/mock.go
@@ -1,13 +1,21 @@
 package mocking
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
+const lastWord = "Go"
+const countdownStart = 3
+
 func main() {
+	Countdown(os.Stdout)
 }
 
-func Countdown(out *bytes.Buffer) {
-	fmt.Fprint(out, "3")
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, lastWord)
 }
